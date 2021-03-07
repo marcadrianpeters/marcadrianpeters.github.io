34c9dc4a25f08d80fb3ddc1cae41392d3d9cedd0
Marc Adrian Peters <marcadrianpeters@gmail.com>
2021-03-07 18:35:51 +0100

    second game

---
+++ js/second_game.js
@@ -0,0 +1,34 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload(){
+    this.load.setBaseURL('https://mapeters.me');
+
+    this.load.image('background', 'png/background.png');
+    this.load.image('pixel', 'png/pixel.png');
+}
+
+function create(){
+    this.add.image(0, 0, 'background');
+}
+
+function update(){
+
+}
